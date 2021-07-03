6ae4209dd8f1ef510bf61026e3bef06ecb447197
Stanislav Panchenko <stanip@hotmail.de>
2021-07-03 13:43:05 +0200

    style(ui/BaseButton): chaged cursor to pointer

--- packages/ui/src/components/buttons/base-button/BaseButton.tsx
+++ packages/ui/src/components/buttons/base-button/BaseButton.tsx
@@ -1,11 +1,16 @@
 import React, {FC, HTMLProps} from 'react';
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import {colors, border} from "../../../theme";
 
-const StyledBaseButton = styled.button`
+const BaseButtonCSS = css`
 	border: none;
+	cursor: pointer;
 	border-radius: ${border('radius.md')};
 	background-color: ${colors('gray.200')};
+`
+
+const StyledBaseButton = styled.button`
+	${BaseButtonCSS}
 `
 
 const BaseButton:FC<HTMLProps<HTMLButtonElement>> = (props) => {
