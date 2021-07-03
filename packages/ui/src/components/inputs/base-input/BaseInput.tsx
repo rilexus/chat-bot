448931dfe5f58cfd55d0021508301d30d7488dc3
Stanislav Panchenko <stanip@hotmail.de>
2021-07-03 17:24:31 +0200

    deps(ui&frontend): prettier installed

--- packages/ui/src/components/inputs/base-input/BaseInput.tsx
+++ packages/ui/src/components/inputs/base-input/BaseInput.tsx
@@ -1,23 +1,24 @@
-import React, {FC, HTMLProps} from 'react';
+import React, { FC, HTMLProps } from "react";
 import styled from "styled-components";
-import {colors} from "../../../theme/colors";
-import {padding} from "../../../theme/padding";
-import {text} from "../../../theme/text";
+import { colors } from "../../../theme/colors";
+import { padding } from "../../../theme/padding";
+import { text } from "../../../theme/text";
 
 const StyledBaseInput = styled.input`
-	border: 1px solid ${colors('gray.900')};
-	padding: ${padding('2')} ${padding('5')};
-	font-size: ${text('base')};
-	outline: none;
-	&:focus {
-		border: 1px solid ${colors('blue.600')};
-	};
-`
+  border: 1px solid ${colors("gray.900")};
+  padding: ${padding("2")} ${padding("5")};
+  font-size: ${text("base")};
+  outline: none;
+  &:focus {
+    border: 1px solid ${colors("blue.600")};
+  }
+`;
 
-const BaseInput: FC<{type?: string, placeholder?: string}> = ({ type = 'text', ...props }) => {
-	return (
-		<StyledBaseInput type={type} {...props} />
-	);
+const BaseInput: FC<{ type?: string; placeholder?: string }> = ({
+  type = "text",
+  ...props
+}) => {
+  return <StyledBaseInput type={type} {...props} />;
 };
 
-export {BaseInput, StyledBaseInput};
+export { BaseInput, StyledBaseInput };
